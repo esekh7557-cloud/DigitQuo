49158dd0266574b215cdd75edce89d2626304a88
Ebrahim <esekh7557@gmail.com>
2026-03-11 17:25:56 +0530

    new fix

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,6 @@
-const SUPABASE_URL = "hhttps://umflohaswnlwzrqbzmxs.supabase.co";
+const SUPABASE_URL = "https://umflohaswnlwzrqbzmxs.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtZmxvaGFzd25sd3pycWJ6bXhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyMjMwMDMsImV4cCI6MjA4ODc5OTAwM30.XH45rX-2RW_gDAnN4gAm3O-P_b9jRyyvmoy2iuwJrfA";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_BqSIT0GcmJ54v0PHVf7Hxw_COvYC9L7";
 
 (function initDigtiQuoAuth(global) {
   function hasValidConfig() {
@@ -39,12 +40,16 @@ const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
       }
 
       if (!this.client) {
-        this.client = global.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-          auth: {
-            persistSession: true,
-            autoRefreshToken: true,
-          },
-        });
+        this.client = global.supabase.createClient(
+          SUPABASE_URL,
+          SUPABASE_PUBLISHABLE_KEY || SUPABASE_ANON_KEY,
+          {
+            auth: {
+              persistSession: true,
+              autoRefreshToken: true,
+            },
+          }
+        );
       }
 
       return this.client;
